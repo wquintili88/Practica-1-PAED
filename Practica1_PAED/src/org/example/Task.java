b00af3d257d223bb92a5ce7c0f0ecf1bea3b1d97
Martí Polo Ortiz <marti.polo@student.salle.url.edu>
2024-11-12 13:18:43 +0100

    inicial

---
+++ Practica1_PAED/src/org/example/Task.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Task {
+    private String name;
+    private String date_limit;
+    private int temps;
+    private int dificultat;
+    private int progress;
+    private String color;
+
+
+
+}
